f94485de296c088f64108e103240195ab88be012
ibanunmangun <62372980+ibanunmangun@users.noreply.github.com>
2022-06-24 22:58:18 +0800

    Add files via upload

---
+++ data/points_2.js
@@ -0,0 +1,11 @@
+var json_points_2 = {
+"type": "FeatureCollection",
+"name": "points_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "Museum Gajah", "desc": "Museum Nasional Indonesia, Jl. Medan Merdeka Barat No.12, Gambir, Central Jakarta City, Jakarta 10110, Indonesia", "X": "106.821590", "Y": "-6.1764021" }, "geometry": { "type": "Point", "coordinates": [ 106.8215901, -6.1764021 ] } },
+{ "type": "Feature", "properties": { "name": "Distrik Seni Sarinah", "desc": "Sarinah, Jl. M.H. Thamrin No.11, RT.8\/RW.4, Gondangdia, Kec. Menteng, Kota Jakarta Pusat, Daerah Khusus Ibukota Jakarta 10350, Indonesia", "X": "106.823921", "Y": "-6.1877696" }, "geometry": { "type": "Point", "coordinates": [ 106.8239218, -6.1877696 ] } },
+{ "type": "Feature", "properties": { "name": "Perpustakaan Nasional RI", "desc": "Perpustakaan Nasional RI, Jl. Salemba Raya No.28A, RT.5\/RW.6, Kenari, Kec. Senen, Kota Jakarta Pusat, Daerah Khusus Ibukota Jakarta 10430, Indonesia", "X": "106.851320", "Y": "-6.1987918" }, "geometry": { "type": "Point", "coordinates": [ 106.8513205, -6.1987918 ] } },
+{ "type": "Feature", "properties": { "name": "Toko Buku Kwitang", "desc": "toko buku kwitang, Jl. Kramat Raya No.14, RT.2\/RW.7, Kwitang, Kec. Senen, Kota Jakarta Pusat, Daerah Khusus Ibukota Jakarta 10420, Indonesia", "X": "106.842091", "Y": "-6.1796752" }, "geometry": { "type": "Point", "coordinates": [ 106.8420918, -6.1796752 ] } }
+]
+}
